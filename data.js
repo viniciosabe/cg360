d2712cd74d1cb45f3ed8d0fb6aa409f5d998e6dc
Vinicios da Silva Abe <viniciosabe61@gmail.com>
2023-10-27 09:30:28 -0400

    init

---
+++ data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-praa-ary-coelho----01",
+      "name": "Praça Ary Coelho  - 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3737.5,
+      "initialViewParameters": {
+        "yaw": -0.05416113687554436,
+        "pitch": 0.014672245989450161,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2114866249000169,
+          "pitch": 0.046842806949911164,
+          "rotation": 0,
+          "target": "1-praa-ary-coelho----02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3504990830999155,
+          "pitch": -0.02963186593199829,
+          "title": "Praça Ary Coelho&nbsp;",
+          "text": "A Praça Ary Coelho fica situada no Centro de Campo Grande. O local abrigou o primeiro cemitério de Campo Grande, tornando-se praça em 1909 com o novo traçado da cidade. Em 1954 recebeu o nome de Praça Ary Coelho em homenagem ao Prefeito de Campo Grande Ary Coelho de Oliveira, assassinado em 1952, em Cuiabá-MT. Wikipédia<br><span style=\"color: rgb(77, 81, 86); font-family: arial, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\"></span>"
+        }
+      ]
+    },
+    {
+      "id": "1-praa-ary-coelho----02",
+      "name": "Praça Ary Coelho  - 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3737.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9559967601461778,
+          "pitch": 0.0037830379560546135,
+          "rotation": 0,
+          "target": "0-praa-ary-coelho----01"
+        },
+        {
+          "yaw": 3.006788841001657,
+          "pitch": -0.017589229301707476,
+          "rotation": 0,
+          "target": "2-praa-ary-coelho---03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-praa-ary-coelho---03",
+      "name": "Praça Ary Coelho - 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3737.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1850890465120898,
+          "pitch": 0.015620938529210093,
+          "rotation": 0,
+          "target": "1-praa-ary-coelho----02"
+        },
+        {
+          "yaw": 2.4791345358690062,
+          "pitch": 0.07236712444854732,
+          "rotation": 0,
+          "target": "0-praa-ary-coelho----01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
